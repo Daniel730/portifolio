9970732937b6d5cecc790fb31a66cfac88dfcc3c
Dan Silva <daniel.730@outlook.com>
2022-04-15 00:27:31 +0100

    dps: new navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,28 +5,37 @@ import "./index.scss"
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="miniature">
-        <label>Daniel Silva</label>
-      </div>
-      <ul>
-        <li>
-          <a href="#about" className="navLink">
-            Sobre mim
-          </a>
-        </li>
-        <li>
-          <a href="#projects" className="navLink">
-            Meus projetos
-          </a>
-        </li>
-        <li>
-          <a href="#contact" className="navLink">
-            Entre em Contato
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div className="navbar">
+      <div className="left-side" />
+      <a href="#" className="animated">
+        <span />
+        <span />
+        <span />
+        <span />
+        Home
+      </a>
+      <a href="#" className="animated">
+        <span />
+        <span />
+        <span />
+        <span />
+        Sobre
+      </a>
+      <a href="#" className="animated">
+        <span />
+        <span />
+        <span />
+        <span />
+        Projetos
+      </a>
+      <a href="#" className="animated">
+        <span />
+        <span />
+        <span />
+        <span />
+        Contato
+      </a>
+    </div>
   )
 }
 
