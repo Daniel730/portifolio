798b9d47fca8d71b165e5e66f39025ed55523560
Dan Silva <daniel.730@outlook.com>
2022-04-15 01:41:47 +0100

    dps: new social icons

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react"
-import { Fade, LightSpeed, Bounce } from "react-reveal"
+import { Fade, Bounce } from "react-reveal"
 import Typewriter from "typewriter-effect"
 
 import "./index.scss"
@@ -10,54 +10,65 @@ import linkedin from "../../assets/imgs/linkedin.png"
 import logo from "../../assets/imgs/logo.png"
 
 function About() {
+  const typeWriter = (typewriter) => {
+    typewriter
+      .pauseFor(1000)
+      .typeString("Sou Fron")
+      .pauseFor(200)
+      .deleteChars(4)
+      .typeString("Back")
+      .pauseFor(200)
+      .deleteChars(4)
+      .typeString("FullStack Developer")
+      .pauseFor(2500)
+      .deleteAll()
+      .start()
+  }
   return (
     <div className="introDiv">
       <div className="intro">
         <Fade top cascade duration={3000}>
-          <LightSpeed cascade>
-            <h1>
-              Olá, meu nome é <br />
-              <strong>Daniel Silva!</strong>
-              <br />
-              <span>
-                <Typewriter
-                  options={{
-                    autoStart: true,
-                    loop: true,
-                  }}
-                  onInit={(typewriter) => {
-                    typewriter
-                      .pauseFor(1000)
-                      .typeString("Sou Fron")
-                      .pauseFor(200)
-                      .deleteChars(4)
-                      .typeString("Back")
-                      .pauseFor(200)
-                      .deleteChars(4)
-                      .typeString("FullStack Developer")
-                      .pauseFor(2500)
-                      .deleteAll()
-                      .start()
-                  }}
-                />
-              </span>
-            </h1>
-          </LightSpeed>
+          <h1>
+            Olá, meu nome é <br />
+            <strong>Daniel Silva!</strong>
+            <br />
+            <span>
+              <Typewriter
+                options={{
+                  autoStart: true,
+                  loop: true,
+                }}
+                onInit={typeWriter}
+              />
+            </span>
+          </h1>
         </Fade>
-        <div className="social">
-          <a
-            target="__blank"
-            href="https://www.linkedin.com/in/daniel-pereira-developer/"
-          >
-            <img src={linkedin} alt="linkedin" />
-          </a>
-          <a target="__blank" href="https://www.instagram.com/oieusouodann/">
-            <img src={instagram} alt="instagram" />
-          </a>
-          <a target="__blank" href="https://github.com/Daniel730">
-            <img src={github} alt="github" />
-          </a>
-        </div>
+        <ul className="wrapper">
+          <li className="icon">
+            <span>Sociais: </span>
+          </li>
+          <li className="icon linkedin">
+            <span className="tooltip">Linkedin</span>
+            <a
+              target="__blank"
+              href="https://www.linkedin.com/in/daniel-pereira-developer/"
+            >
+              <img src={linkedin} alt="linkedin" />
+            </a>
+          </li>
+          <li className="icon instagram">
+            <span className="tooltip">Instagram</span>
+            <a target="__blank" href="https://www.instagram.com/oieusouodann/">
+              <img src={instagram} alt="instagram" />
+            </a>
+          </li>
+          <li className="icon github">
+            <span className="tooltip">Github</span>
+            <a target="__blank" href="https://github.com/Daniel730">
+              <img src={github} alt="github" />
+            </a>
+          </li>
+        </ul>
       </div>
       <Bounce>
         <div className="animation">
