aa52f58c1fdebc48288f122072f7fe8391170986
Daniel Silva <daniel.730@outlook.com>
2022-09-15 00:05:34 +0100

    dps: New navbar test

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,42 +1,29 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react"
+
 import "./index.scss"
+import NavLink from "./NavLink"
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <div className="left-side" />
-      <a href="#" className="animated">
-        <span />
-        <span />
-        <span />
-        <span />
-        Home
-      </a>
-      <a href="#" className="animated">
-        <span />
-        <span />
-        <span />
-        <span />
-        Sobre
-      </a>
-      <a href="#" className="animated">
-        <span />
-        <span />
-        <span />
-        <span />
-        Projetos
-      </a>
-      <a href="#" className="animated">
-        <span />
-        <span />
-        <span />
-        <span />
-        Contato
+    <div className="navbar" id="navbar">
+      <NavLink href="teste" text="Teste" active />
+      <NavLink href="teste" text="Teste" />
+      <NavLink href="teste" text="Teste" />
+      <NavLink href="teste" text="Teste" />
+      <a href="#hamb" className="icon" onClick={() => myFunction()}>
+        <i className="fa fa-bars" />
       </a>
     </div>
   )
+}
+
+function myFunction() {
+  const x = document.getElementById("navbar")
+  if (x.className === "navbar") {
+    x.className += " responsive"
+  } else {
+    x.className = "navbar"
+  }
 }
 
 export default Navbar
